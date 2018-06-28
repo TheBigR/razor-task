772df020f68ae14b3a90cc7b64c5a483949f553a
thebigr <nevoroy@gmail.com>
2018-06-28 13:49:09 +0300

    started answering 2nd question - http get is up and running.

--- src/app/task-detail/task-detail.component.ts
+++ src/app/task-detail/task-detail.component.ts
@@ -3,6 +3,8 @@ import {Task} from '../task';
 import {Location} from '@angular/common';
 import {ActivatedRoute} from '@angular/router';
 import {TaskService} from '../services/task.service';
+import {NorthwindService} from '../services/northwind.service';
+import {NorthWindData} from '../northWindData';
 
 @Component({
   selector: 'app-task-detail',
@@ -14,10 +16,14 @@ import {TaskService} from '../services/task.service';
 export class TaskDetailComponent implements OnInit {
 
   @Input() task: Task;
-  constructor(private route: ActivatedRoute, private taskService: TaskService, private location: Location) { }
-
+  constructor(private route: ActivatedRoute,
+              private taskService: TaskService,
+              private location: Location,
+              private northWindService: NorthwindService) { }
+  windData: NorthWindData[];
   ngOnInit() {
     this.getTask();
+    this.getNwData();
   }
   getTask(): void {
     const id =  +this.route.snapshot.paramMap.get('id');
@@ -26,6 +32,15 @@ export class TaskDetailComponent implements OnInit {
 
   goBack(): void {
     this.location.back();
+  }
+
+  getSecondAnswer(): void {
+    console.log('this is the second answer func calling');
+    console.log(this.windData);
+  }
+
+  getNwData(): void {
+    this.northWindService.getData().subscribe( windData => this.windData = windData);
   }
 
 }
